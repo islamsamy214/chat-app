747560c5677d37f5acb8a99e20cdf4379bd2b852
unknown <islamsamy214@gmai.com>
2021-08-20 17:20:56 +0200

    update commit

--- resources/js/Pages/Chat/MessageItem.vue
+++ resources/js/Pages/Chat/MessageItem.vue
@@ -1,11 +1,46 @@
 <template>
-    <div>
-        {{ message.user.name }}: {{ message.message }}
+  <div>
+    <div
+      class="
+        bg-green-500
+        shadow-md
+        m-3
+        p-3
+        text-white
+        rounded-br-lg
+        min-h-full
+        w-auto
+        flex
+        items-start
+        justify-start
+      "
+      v-if="message.user.id == $page.props.auth.user.id"
+    >
+      {{ message.user.name }}: {{ message.message }}
     </div>
+    <div
+      class="
+        bg-blue-500
+        shadow-md
+        m-3
+        p-3
+        text-white
+        rounded-bl-lg
+        min-h-full
+        w-auto
+        flex
+        items-end
+        justify-end
+      "
+      v-else
+    >
+      {{ message.message }} :{{ message.user.name }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props:['message']
-}
+  props: ["message"],
+};
 </script>
